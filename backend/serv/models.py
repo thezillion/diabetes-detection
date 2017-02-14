3fea961c242762e70abdb0076617ce222e9fea5a
Riza <rza108@gmail.com>
2017-02-15 02:18:54 +0530

    grr

--- backend/serv/models.py
+++ backend/serv/models.py
@@ -1,5 +1,31 @@
-from __future__ import unicode_literals
+
 
 from django.db import models
+
+
+class Person(models.Model):
+	def __init__(self,name, age, insulin, gender, email, bmi,bp, glucose):
+		self.name=name
+		self.age=age
+		self.gender=gender
+		self.email=email
+		self.bmi=bmi
+		self.bp=bp
+		self.glucose=glucose
+
+	name=models.CharField(max_length=20)
+	age=models.IntegerField(default=0)
+	insulin=models.IntegerField(default=0)
+	GENDER_CHOICES=(
+		('M','Male'),
+		('F','Female')
+		)
+	gender=models.CharField(max_length=1, choices= GENDER_CHOICES)
+
+	email=models.CharField(max_length=20)
+	bmi=models.IntegerField(default=0)
+	bp=models.IntegerField(default=0)
+	glucose=models.IntegerField(default=0)
+	
 
 # Create your models here.
--- backend/serv/views.py
+++ backend/serv/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
+from .models import Person
+from .classification import Classify
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+	if request.method== 'POST':
+
+		name= request.POST.get('name', -1)
+		age= request.POST.get('age', -1)
+		insulin= request.POST.get('insulin',-1)
+		gender=request.POST.get('gender',-1)
+		email=request.POST.get('email',-1)
+		bmi=request.POST.get('bmi',-1)
+		bp=request.POST.get('bp',-1)
+		glucose=request.POST.get('glu',-1)
+
+		p=Person(name,age, insulin, gender, email, bmi,bp)
+		classify=Classify('data.csv')
+		return HttpResponse(classify.predict(glucose,bp,insulin,bmi))
+
+
+
+
+		
+	else:
+		return HttpResponse("NO POST")
